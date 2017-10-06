4edae840151dacd226c714c8f482fc941947c6db
Joe Rabinoff <jrabinoff6@math.gatech.edu>
2017-10-06 10:56:08 -0400

    Updated section 2.1.

--- makefigs.sh
+++ makefigs.sh
@@ -4,7 +4,10 @@ compile_dir="$(cd "$(dirname "$0")"; pwd)"
 base_dir="$compile_dir/.."
 mbx="$base_dir/mathbook/script/mbx"
 
+xmlfile=linalg.xml
+[ -n "$1" ] && xmlfile="$1"
+
 cd "$compile_dir"
 TEXINPUTS=".:$compile_dir/style:"; export TEXINPUTS
 TEXMFHOME="$compile_dir/style/texmf-var"; export TEXMFHOME
-exec $mbx -c latex-image -f svg -d images linalg.xml
+exec $mbx -c latex-image -f svg -d images "$xmlfile"
